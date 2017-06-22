8e0b03cceb8dd359c7caea56ee40501172c13df8
Aseem Arora <aseemarora1995@gmail.com>
2017-06-22 19:12:14 +0530

    uses binary search

---
+++ Leetcode/(M)(binary search)33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int binarySearch(int l, int r, int target, vector<int>& nums){
+        int idx = r+l;
+        idx /= 2;
+        if(l > r){
+            return -1;
+        }
+        if(nums[idx] == target){
+            return idx;
+        }
+        else if(nums[idx] > target){
+            return binarySearch(l, r-1, target, nums);
+        }
+        else{
+            return binarySearch(l+1, r, target, nums);
+        }
+    }
+    
+    int search(vector<int>& nums, int target) {
+        if(nums.empty()){
+            return -1;
+        }
+        int l = 0, r = l + 1;
+        
+        while(r != nums.size()){
+            if(nums[r] < nums[l]){
+                break;
+            }
+            l++;
+            r++;
+        }
+        cout << "pivot: " << r << endl;
+        
+        int res = binarySearch(0, r-1, target, nums);
+        if(res == -1)
+            res = binarySearch(r, nums.size()-1, target, nums);
+        
+        return res;
+    }
+};
