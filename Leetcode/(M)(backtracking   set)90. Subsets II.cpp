46a998811add40658f4986dfeb9f6350e5ac1161
Aseem Arora <aseemarora1995@gmail.com>
2017-06-27 04:23:49 +0530

    implements backtracking. set to remove duplicates.

---
+++ Leetcode/(M)(backtracking   set)90. Subsets II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void print(vector<int>& temp){
+        for(auto i: temp){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    void subsetsUtil(int idx, vector<int>& nums, vector<int>& temp, vector<vector<int>>& res, int call){
+        //cout << "function call: " << call << endl; call++;
+        if(idx == nums.size()){
+            res.push_back(temp);
+            //cout << "adding: ";
+            print(temp);
+            return; 
+        }
+        for(int i=idx; i<nums.size(); i++){
+            //cout << "pushing: " << nums[i] << endl;
+            temp.push_back(nums[i]);
+            subsetsUtil(i+1, nums, temp, res, call);
+            //print(temp);
+            //cout << "->popping: " << *(temp.end()-1) << endl;
+            temp.pop_back();
+            //print(temp);
+        }
+        //cout << "ends.." << endl;
+        res.push_back(temp);
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> res;
+        int call = 1;
+        sort(nums.begin(), nums.end());
+        subsetsUtil(0, nums, temp, res, call);
+        set<vector<int> > mySet(res.begin(), res.end());
+        vector<vector<int> > res1(mySet.begin(), mySet.end());
+        return res1;
+    }
+};
