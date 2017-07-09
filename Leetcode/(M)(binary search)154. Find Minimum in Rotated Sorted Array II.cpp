e920d53f8148174b09718ca5dc06dd86a38aab9e
Aseem Arora <aseemarora1995@gmail.com>
2017-07-09 07:22:56 +0530

    complexity?

---
+++ Leetcode/(M)(binary search)154. Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findMinUtil(int l, int r, vector<int>& nums){
+        if(nums[l] < nums[r] || l==r) return nums[l];
+        int idx = (l+r)/2;
+        if(nums[idx] < nums[l]) return findMinUtil(l, idx, nums);
+        if(nums[idx] > nums[l]) return findMinUtil(idx+1, r, nums);
+        else return findMinUtil(l+1, r, nums);
+    }
+    int findMin(vector<int>& nums) {
+        return findMinUtil(0, nums.size()-1, nums);    
+    }
+};
